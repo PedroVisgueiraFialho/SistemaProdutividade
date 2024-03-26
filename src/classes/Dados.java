1736e4bf520d2f7908ae6a70a9e738b2797cd8b9
PedroVisgueiraFialho <visgueiraft@gmail.com>
2024-03-26 18:21:36 -0300

    commit1

---
+++ src/classes/Dados.java
@@ -0,0 +1,14 @@
+
+package classes;
+
+public class Dados {
+    public boolean validarUsuario (String Cpf, String Dn){
+        if(Cpf.equals("11122233344")&& Dn.equals("010290")){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+}
+
---
+++ src/classes/JavaSistema.java
@@ -0,0 +1,16 @@
+
+package classes;
+
+import formularios.frmLogin;
+
+public class JavaSistema {
+
+    public static void main(String[] args) {
+     frmLogin miLogin = new frmLogin();
+     miLogin.setLocationRelativeTo(null);
+     miLogin.setVisible(true);
+    
+    
+    }
+    
+}
